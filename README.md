6abde56853dec704c56dbf1a30a0b30a11749625
Ben Foxall <benfoxall@gmail.com>
2013-07-23 23:36:08 +0100

    allow deploying on heroku

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ Example of using phantomjs webserver module
 ```bash
 phantomjs server.js
 ```
+
+## Deploying
+
+This can be deployed to heroku with the [phantomjs buildpack](https://github.com/stomita/heroku-buildpack-phantomjs).
+
+```bash
+# create a new heroku app
+heroku create --stack cedar --buildpack http://github.com/stomita/heroku-buildpack-phantomjs.git
+
+# deploy
+git push heroku master
+```
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var server = require('webserver').create();
+var server = require('webserver').create(),
+	system = require('system'),
+	port   = system.env.PORT || 8080;
 
-var service = server.listen(8080, function(request, response) {
+var service = server.listen(port, function(request, response) {
     response.statusCode = 200;
     response.write('<html><body>Hello!</body></html>');
     response.close();
