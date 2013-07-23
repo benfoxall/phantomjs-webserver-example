f25f04f8fc2f00a41afcd7ee2a1d598e6f6790bb
Ben Foxall <benfoxall@gmail.com>
2013-07-23 23:44:21 +0100

    stubbed html form

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var server = require('webserver').create(),
 	system = require('system'),
+	fs     = require('fs'),
 	port   = system.env.PORT || 8080;
 
 var service = server.listen(port, function(request, response) {
     response.statusCode = 200;
-    response.write('<html><body>Hello!</body></html>');
+    response.write(fs.read('index.html'));
     response.close();
 });
+
