bf1a0a97b4a1ed917e508dd528a19eb4602ece62
Ben Foxall <benfoxall@gmail.com>
2013-07-27 13:27:44 +0100

    added page loading functionality

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ var server = require('webserver').create(),
 	fs     = require('fs'),
 	port   = system.env.PORT || 8080;
 
-var service = server.listen(port, function(request, response) {
+server.listen(port, function(request, response) {
 
 	if(request.method == 'POST'){
 		var url = request.post.url;
-		response.statusCode = 200;
-		// todo: stuff
-		response.write("ok: " + url);
-		response.close();
+		
+		load(url, function(properties, imageuri){
+			response.statusCode = 200;
+			response.write(JSON.stringify(properties));	
+			response.write("\n");	
+			response.write(imageuri);
+			response.close();
+		})
 
 	} else {
 		response.statusCode = 200;
@@ -20,4 +24,39 @@ var service = server.listen(port, function(request, response) {
 
 });
 
-if(service) console.log("server started - http://localhost:" + server.port)
+if(service) console.log("server started - http://localhost:" + server.port);
+
+function load(url, callback){
+	var page = new WebPage();
+
+	page.onLoadStarted = function () {
+		console.log('loading:' + url);
+	};
+
+	page.onLoadFinished = function (status) {
+		console.log('loaded:' + url);
+
+		var properties = page.evaluate(function () {
+			return {
+				title:document.title,
+				links:Object.keys(
+						[].reduce.call(
+							document.querySelectorAll('a'), 
+							function(memo, a){
+								if(link.protocol.indexOf('http') === 0)memo[a.href] = true;
+								return memo;
+							}
+						,{})
+					)
+			}
+		});
+
+		var imageuri = 'data:image/png;base64,' + page.renderBase64('png');
+
+		callback(properties,imageuri);
+
+		page.close();
+	};
+  page.open(url);
+
+}
