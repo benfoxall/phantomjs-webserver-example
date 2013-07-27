f419e24279bb9ef5e4d64fbe96e968969d1f9bdd
Ben Foxall <benfoxall@gmail.com>
2013-07-27 11:55:21 +0100

    use post request for easier parsing

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ var server = require('webserver').create(),
 	port   = system.env.PORT || 8080;
 
 var service = server.listen(port, function(request, response) {
-    response.statusCode = 200;
-    response.write(fs.read('index.html'));
-    response.close();
+
+	if(request.method == 'POST'){
+		var url = request.post.url;
+		response.statusCode = 200;
+		// todo: stuff
+		response.write("ok: " + url);
+		response.close();
+
+	} else {
+		response.statusCode = 200;
+		response.write(fs.read('index.html'));
+		response.close();
+	}
+
 });
 
 if(service) console.log("server started - http://localhost:" + server.port)
